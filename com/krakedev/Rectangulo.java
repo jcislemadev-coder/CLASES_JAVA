636feb4f58522481cc64d5e272ad65f3364378b7
jcislemadev-coder <jcislemadev@gmail.com>
2026-07-31 07:21:04 -0500

    Inclusion de calculo de perimetros del rectangulo

--- com/krakedev/Rectangulo.java
+++ com/krakedev/Rectangulo.java
@@ -8,4 +8,9 @@ public class Rectangulo {
 		int area = base * altura;
 		return area;
 	}
+	
+	public double calcularPerimetro() {
+		double perimetro = (base*2)+(altura *2);
+		return perimetro;
+	}
 }
--- com/krakedev/RectanguloTest.java
+++ com/krakedev/RectanguloTest.java
@@ -15,15 +15,24 @@ public class RectanguloTest {
 		resultadoarea = r1.calcularArea();
 		System.out.println("El resultado es: " + resultadoarea);
 
+		double resultadoperimetro;
+
+		resultadoperimetro = r1.calcularPerimetro();
+		System.out.println("El perimetro es: " + resultadoperimetro);
+
 		Rectangulo r2 = new Rectangulo();
-		
+
 		r2.altura = 8;
 		r2.base = 2;
-		
+
 		int resultador2;
-		
+		double perimetro2;
+
 		resultador2 = r2.calcularArea();
+		perimetro2 = r2.calcularPerimetro();
+
 		System.out.println("El resultado del segundo rectangulo es: " + resultador2);
+		System.out.println("El perimetro es: " + perimetro2);
 
 	}
 
